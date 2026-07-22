ccef0d9b1793dd971f7261bd1f2349c944173fae
agnilrooc-star <agnil.rooc@gmail.com>
2026-07-22 14:53:29 +0800

    Update roster.js

--- js/roster.js
+++ js/roster.js
@@ -1,1 +1,20 @@
-//coming soon
+function renderRoster(){
+
+    const table=document.getElementById("rosterTable");
+
+    table.innerHTML="";
+
+    guildMembers.forEach(player=>{
+
+        table.innerHTML += `
+        <div class="player-card">
+            <strong>${player.ign}</strong><br>
+            ${player.class}<br>
+            ${player.role}<br>
+            Gear: ${player.gear}
+        </div>
+        `;
+
+    });
+
+}
