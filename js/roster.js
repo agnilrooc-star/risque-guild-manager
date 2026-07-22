6a6d3906b02067fad724538604c8eb6f16e0390d
agnilrooc-star <agnil.rooc@gmail.com>
2026-07-22 16:03:33 +0800

    Update roster.js

--- js/roster.js
+++ js/roster.js
@@ -1,60 +1,11 @@
-function getClassImage(playerClass){
-
-    return `assets/classes/${playerClass.toLowerCase()}.png`;
-
-}
+alert("Roster.js Loaded");
 
 function renderRoster(){
 
-    console.log("renderRoster called");
-    console.log(guildMembers);
+    alert("renderRoster() called");
 
     const table = document.getElementById("rosterTable");
 
-    table.innerHTML = "<h3>TEST</h3>";
-
-}
-
-    
-
-    guildMembers.forEach(player=>{
-
-        const card = document.createElement("div");
-
-        card.className = `player-card ${player.class.toLowerCase()}`;
-
-        card.innerHTML = `
-
-            ${player.elite ? '<div class="elite">⭐</div>' : ''}
-
-            <img
-                class="player-icon"
-                src="${getClassImage(player.class)}"
-                alt="${player.class}"
-            >
-
-            <div class="player-name">
-
-                ${player.ign}
-
-            </div>
-
-            <div class="player-role">
-
-                ${player.role}
-
-            </div>
-
-            <div class="player-gs">
-
-                🏆 ${player.gear}
-
-            </div>
-
-        `;
-
-        table.appendChild(card);
-
-    });
+    table.innerHTML = "<h1 style='color:white;'>IT WORKS</h1>";
 
 }
