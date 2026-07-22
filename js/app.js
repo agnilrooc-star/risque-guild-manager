07bcaa284f0a1a4621e2f98841cdfd8b7fc13163
agnilrooc-star <agnil.rooc@gmail.com>
2026-07-22 14:53:01 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 
+function renderPlayerPool(){
+
+    const pool = document.getElementById("playerPool");
+
+    pool.innerHTML = "";
+
+    playerPool.forEach(player=>{
+
+        const card=document.createElement("div");
+
+        card.className="player-card";
+
+        card.innerHTML=`
+            <strong>${player.ign}</strong><br>
+            ${player.class}<br>
+            GS: ${player.gear}
+        `;
+
+        pool.appendChild(card);
+
+    });
+
+}
