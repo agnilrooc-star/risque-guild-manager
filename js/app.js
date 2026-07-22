2e7ec531542c040ceb8f1bb7f1f5b66f48ec4181
agnilrooc-star <agnil.rooc@gmail.com>
2026-07-22 15:33:04 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+function getClassImage(playerClass){
+
+    return `assets/classes/${playerClass.toLowerCase()}.png`;
+
+}
+
+function getClassCSS(playerClass){
+
+    return playerClass.toLowerCase();
+
+}
 
 function renderPlayerPool(){
 
@@ -5,20 +16,48 @@ function renderPlayerPool(){
 
     pool.innerHTML = "";
 
+    document.getElementById("playerCount").innerText =
+        `${playerPool.length} Players`;
+
     playerPool.forEach(player=>{
 
-        const card=document.createElement("div");
+        const card = document.createElement("div");
 
-        card.className="player-card";
+        card.className = `player-card ${getClassCSS(player.class)}`;
 
-        card.innerHTML=`
-            <strong>${player.ign}</strong><br>
-            ${player.class}<br>
-            GS: ${player.gear}
+        card.innerHTML = `
+
+            ${player.elite ? '<div class="elite">⭐</div>' : ''}
+
+            <img
+                class="player-icon"
+                src="${getClassImage(player.class)}"
+                alt="${player.class}"
+            >
+
+            <div class="player-name">
+
+                ${player.ign}
+
+            </div>
+
+            <div class="player-role">
+
+                ${player.role}
+
+            </div>
+
+            <div class="player-gs">
+
+                🏆 ${player.gear}
+
+            </div>
+
         `;
 
         pool.appendChild(card);
 
     });
 
+}
 }
