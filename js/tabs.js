74e47eb4352958ec7573eb22ba2acf4f936ff068
agnilrooc-star <agnil.rooc@gmail.com>
2026-07-22 15:00:46 +0800

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -1,1 +1,25 @@
-//coming soon
+const plannerTab = document.getElementById("plannerTab");
+const rosterTab = document.getElementById("rosterTab");
+
+const plannerPage = document.getElementById("plannerPage");
+const rosterPage = document.getElementById("rosterPage");
+
+plannerTab.onclick = () => {
+
+    plannerTab.classList.add("active");
+    rosterTab.classList.remove("active");
+
+    plannerPage.style.display = "flex";
+    rosterPage.style.display = "none";
+
+};
+
+rosterTab.onclick = () => {
+
+    rosterTab.classList.add("active");
+    plannerTab.classList.remove("active");
+
+    rosterPage.style.display = "block";
+    plannerPage.style.display = "none";
+
+};
