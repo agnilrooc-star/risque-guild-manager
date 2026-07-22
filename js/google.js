e155dcec92cc0072e7ca60dc1d82c0b8f3a2e33f
agnilrooc-star <agnil.rooc@gmail.com>
2026-07-22 14:52:43 +0800

    Update google.js

--- js/google.js
+++ js/google.js
@@ -1,1 +1,51 @@
-//coming soon
+// =====================================
+// GOOGLE SHEETS CONFIG
+// =====================================
+
+const API_URL = "https://script.google.com/macros/s/AKfycbxnepH5bglAVBTaeUBddyb-U8ptc7s3PpurUYRLRFGUG3jNfC2druVqQ--9wt0vqtaPpg/exec";
+
+let guildMembers = [];
+let playerPool = [];
+
+// =====================================
+// LOAD DATA
+// =====================================
+
+async function loadGuildMembers(){
+
+    try{
+
+        const response = await fetch(API_URL);
+
+        const data = await response.json();
+
+        guildMembers = data;
+
+        playerPool = guildMembers.filter(player => {
+
+            return player.active === true ||
+                   player.active === "TRUE" ||
+                   player.active === "true";
+
+        });
+
+        renderPlayerPool();
+        renderRoster();
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+    }
+
+
+}
+// Initial load
+loadGuildMembers();
+
+// Refresh every 5 seconds
+setInterval(loadGuildMembers,5000);
+
+
